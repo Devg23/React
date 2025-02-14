bbff61daae3534fff4ea45d8aed5fce0273b46d2
Devg23 <devashishg996@gmail.com>
2025-02-14 15:56:27 +0530

    My second commit

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,5 +1,11 @@
+import Chai from "./chai.js";
 function App() {
-  return <h1>Hello World | Devashish Gupta</h1>;
+  return (
+    <>
+      <h1>Hello World | Devashish Gupta</h1>
+      <Chai />
+    </>
+  );
 }
 
 export default App;
---
+++ firstapp/src/chai.js
@@ -0,0 +1,5 @@
+function Chai() {
+  return <h2>Chai in JS</h2>;
+}
+
+export default Chai;
